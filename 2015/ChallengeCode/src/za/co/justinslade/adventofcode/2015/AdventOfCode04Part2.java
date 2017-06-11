bd1ae46842fbb4033ef5529c09baefc80220f50d
Justin Slade <slade.justin@gmail.com>
2017-06-11 12:28:08 +0100

    adds day4

---
+++ 2015/ChallengeCode/src/za/co/justinslade/adventofcode/2015/AdventOfCode04Part2.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class AdventOfCode04Part2 {
+
+    public static String getMD5Hex(final String inputString) throws NoSuchAlgorithmException {
+
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(inputString.getBytes());
+
+        byte[] digest = md.digest();
+
+        return convertByteToHex(digest);
+    }
+
+    private static String convertByteToHex(byte[] byteData) {
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < byteData.length; i++) {
+            sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        File file = new File(args[0]);
+        BufferedReader buffer = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = buffer.readLine()) != null) {
+            line = line.trim();
+            String hash = "";
+            int currentNum = 0;
+            while (!hash.startsWith("000000")){
+                currentNum++;
+                hash = getMD5Hex(String.format("%s%d", line, currentNum));
+            }
+            System.out.printf("Lowest number is %d", currentNum);
+        }
+
+    }
+}
